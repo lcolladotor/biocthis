0180aff74053915a8dddb88c73b012e8adbb30da
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-04-29 10:46:16 -0400

    Add git and github setup steps

--- inst/package_dev/01_create_pkg.R
+++ inst/package_dev/01_create_pkg.R
@@ -4,6 +4,9 @@
 usethis::create_package("YOUR_PACKAGE_NAME")
 ## This open a new window in RStudio
 
+## Note: If you create packages frequently, check the help file for
+## usethis::use_description for more information on how to set some R author
+## defaults.
 
 ## Add package development files from biocthis
 biocthis::package_dev_files()
--- inst/package_dev/02_git_github_setup.R
+++ inst/package_dev/02_git_github_setup.R
@@ -3,5 +3,10 @@
 ## Note that Bioconductor doesn't allow *.Rproj files
 ## So we have to ignore it before anything else
 usethis::use_git_ignore("*.Rproj")
-usethis::use_git()
-usethis::use_github("LieberInstitute")
+usethis::use_git() ## Choose the option to make the commit, then to restart RStudio
+
+## After the restart, continue by connecting your local git repository to
+## GitHub. You might want to use the `organisation` and `private` arguments
+args(usethis::use_github)
+usethis::use_github()
+## Follow any prompts, such as running: git push --set-upstream origin master
