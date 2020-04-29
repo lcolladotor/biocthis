59594141c29c1be25d64f4a80adba611a1022e40
Leonardo Collado Torres <lcolladotor@gmail.com>
2020-04-29 14:40:17 -0400

    Create the function use_bioc_pkg_templates()

---
+++ R/use_bioc_pkg_templates.R
@@ -0,0 +1,30 @@
+#' Create a biocthis package template R scripts
+#'
+#' This function creates the `dev` directory and in it, it creates R script
+#' files that you can follow to create your own Bioconductor-friendly R
+#' package.
+#'
+#' @inheritParams create_readme_rmd
+#'
+#' @return
+#' @export
+#' @import usethis
+#' @importFrom fs path
+#'
+#' @examples
+#'
+#'  \dontrun{
+#' use_bioc_pkg_templates()
+#' }
+#'
+use_bioc_pkg_templates <- function(open = rlang::is_interactive()) {
+    data <- usethis:::project_data()
+    dir.create(fs::path("dev"), showWarnings = FALSE)
+    use_build_ignore(fs::path("dev"))
+    use_template("01_create_pkg.R", fs::path("dev", "01_create_pkg.R"),
+        data = data, package = 'biocthis', open = open)
+    use_template("02_git_github_setup.R", fs::path("dev", "02_git_github_setup.R"),
+        data = data, package = 'biocthis', open = open)
+    use_template("03_core_files.R", fs::path("dev", "03_core_files.R"),
+        data = data, package = 'biocthis', open = open)
+}
