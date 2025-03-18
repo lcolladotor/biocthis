537b07b667ee8a65b07088ef837221d4a8f7ac22
lcolladotor <lcolladotor@gmail.com>
2025-03-18 15:50:01 -0400

    don't need fs::path() here

--- R/use_bioc_pkg_templates.R
+++ R/use_bioc_pkg_templates.R
@@ -53,7 +53,7 @@ use_bioc_pkg_templates <- function(open = rlang::is_interactive()) {
     )
     use_directory("dev")
     use_build_ignore(fs::path("dev"))
-    use_git_ignore(fs::path("dev"))
+    use_git_ignore("dev")
     use_template(
         "01_create_pkg.R",
         fs::path("dev", "01_create_pkg.R"),
